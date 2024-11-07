8af0b03bfc3f87a9052805375cc8f1fba388ad76
Lucia Cordone <matactivo.ep@gmail.com>
2024-11-07 07:14:08 +0100

    correccion errores

--- back/app.js
+++ back/app.js
@@ -48,7 +48,7 @@ secured = async (req, res, next) => {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
-app.use('/admin/novedades', secured, adminRouter);
+app.use('/admin/novedades', secured, novedadesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
