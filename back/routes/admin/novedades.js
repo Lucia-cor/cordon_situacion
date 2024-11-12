84b5139ae20ea6466c198893b2d397f28dd788b5
Lucia Cordone <matactivo.ep@gmail.com>
2024-11-12 16:47:31 +0100

    admin, novedades, agregar

--- back/routes/admin/novedades.js
+++ back/routes/admin/novedades.js
@@ -1,11 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require( './../../models/novedadesModel');
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades', {
     layout:'admin/layout',
-    usuario: req.session.nombre
+    usuario: req.session.nombre,
+    novedades
   });
+});
+
+router.get('/agregar', (req,res,next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  });
+});
+
+router.post('/agregar', async (req, res, next) => {
+  try{
+    if (req.body.titulo != "" && req.body.lugar != "" && req.body.cuerpo != ""){
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true, message: 'Todos los campos son requeridos'
+        
+      })
+    }
+  } catch (error){
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se cargó la actividad'
+    });
+  }
 });
 
 
